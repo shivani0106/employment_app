2eb5c15c904f8698d23b95240e47efb03a689102
Fenil <fenilm57@gmail.com>
2021-02-10 22:20:58 +0530

    Sign in with Google and Facebook Done

--- employment_app/lib/auth/SignIn.dart
+++ employment_app/lib/auth/SignIn.dart
@@ -4,6 +4,9 @@ import 'package:employment_app/style/Style.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'ForgotPassword.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:modal_progress_hud/modal_progress_hud.dart';
+import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
 
 class SignIn extends StatefulWidget {
   SignIn({Key key}) : super(key: key);
@@ -18,21 +21,23 @@ class _SignInState extends State<SignIn> {
   FocusNode _focusNodeOne = FocusNode();
   FocusNode _focusNodeTwo = FocusNode();
   bool passwordVisiable;
+  bool showSpinner = false;
+
   final _phoneController = TextEditingController();
   final _codeController = TextEditingController();
 
   // Phone verification function
 
+  FirebaseAuth _auth = FirebaseAuth.instance;
+  UserCredential result;
   Future<bool> loginUser(String phone, BuildContext context) async {
-    FirebaseAuth _auth = FirebaseAuth.instance;
-
     _auth.verifyPhoneNumber(
       phoneNumber: phone,
       timeout: Duration(seconds: 60),
       verificationCompleted: (AuthCredential credential) async {
         Navigator.of(context).pop();
         // Automatic verification of phone number
-        UserCredential result = await _auth.signInWithCredential(credential);
+        result = await _auth.signInWithCredential(credential);
         User user = result.user;
 
         //Navigator PUSH to HomeScreen() in home_screen.dart
@@ -101,6 +106,52 @@ class _SignInState extends State<SignIn> {
     );
   }
 
+  //Google
+  Future<UserCredential> signInWithGoogle() async {
+    // Trigger the authentication flow
+    final GoogleSignInAccount googleUser = await GoogleSignIn().signIn();
+
+    // Obtain the auth details from the request
+    final GoogleSignInAuthentication googleAuth =
+        await googleUser.authentication;
+
+    // Create a new credential
+    final GoogleAuthCredential credential = GoogleAuthProvider.credential(
+      accessToken: googleAuth.accessToken,
+      idToken: googleAuth.idToken,
+    );
+
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => HomeScreen(),
+      ),
+    );
+
+    // Once signed in, return the UserCredential
+    result = await _auth.signInWithCredential(credential);
+
+    return result;
+  }
+
+  void signOutUser() {
+    _auth.signOut();
+  }
+
+  //Facebook
+  Future<UserCredential> signInWithFacebook() async {
+    // Trigger the sign-in flow
+    final AccessToken result = await FacebookAuth.instance.login();
+
+    // Create a credential from the access token
+    final FacebookAuthCredential facebookAuthCredential =
+        FacebookAuthProvider.credential(result.token);
+
+    // Once signed in, return the UserCredential
+    return await FirebaseAuth.instance
+        .signInWithCredential(facebookAuthCredential);
+  }
+
   @override
   void initState() {
     super.initState();
@@ -120,158 +171,187 @@ class _SignInState extends State<SignIn> {
             style: largePrimaryColorBold(),
           ),
         ),
-        body: SingleChildScrollView(
-            child: Container(
-                height: 500,
-                margin: EdgeInsets.only(
-                    left: screenWidth(context) * 0.1,
-                    right: screenWidth(context) * 0.1,
-                    top: screenHeight(context) * 0.1,
-                    bottom: screenHeight(context) * 0.1),
-                child: Form(
-                  key: _formKey,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    crossAxisAlignment: CrossAxisAlignment.stretch,
-                    children: [
-                      // Row(
-                      //   mainAxisAlignment: MainAxisAlignment.center,
-                      //   children: [
-                      //     Container(
-                      //       height: 40,
-                      //       width: screenWidth(context) / 4,
-                      //       child: Image.asset(
-                      //         'lib/assets/images/appLogo.jpg',
-                      //         fit: BoxFit.contain,
-                      //       ),
-                      //     ),
-                      //     Container(
-                      //       child: Text(
-                      //         'Employment',
-                      //         style: largePrimaryColorsemiBold(),
-                      //       ),
-                      //     )
-                      //   ],
-                      // ),
+        body: ModalProgressHUD(
+          inAsyncCall: showSpinner,
+          child: SingleChildScrollView(
+              child: Container(
+                  height: 500,
+                  margin: EdgeInsets.only(
+                      left: screenWidth(context) * 0.1,
+                      right: screenWidth(context) * 0.1,
+                      top: screenHeight(context) * 0.1,
+                      bottom: screenHeight(context) * 0.1),
+                  child: Form(
+                    key: _formKey,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        // Row(
+                        //   mainAxisAlignment: MainAxisAlignment.center,
+                        //   children: [
+                        //     Container(
+                        //       height: 40,
+                        //       width: screenWidth(context) / 4,
+                        //       child: Image.asset(
+                        //         'lib/assets/images/appLogo.jpg',
+                        //         fit: BoxFit.contain,
+                        //       ),
+                        //     ),
+                        //     Container(
+                        //       child: Text(
+                        //         'Employment',
+                        //         style: largePrimaryColorsemiBold(),
+                        //       ),
+                        //     )
+                        //   ],
+                        // ),
 
-                      ///Mobile no ****************************************************************************
-                      TextFormField(
-                        validator: validateMobile,
-                        autofocus: false,
-                        controller: _phoneController,
-                        focusNode: _focusNodeOne,
-                        keyboardType: TextInputType.phone,
-                        decoration: InputDecoration(
-                          prefix: Text("$countryCode   "),
-                          labelText: 'Mobile No.',
-                          labelStyle: _focusNodeOne.hasFocus
-                              ? regularBlueColor()
-                              : regulargreyColor(),
-                          //  errorText: validateMobile.toString()
+                        ///Mobile no ****************************************************************************
+                        TextFormField(
+                          validator: validateMobile,
+                          autofocus: false,
+                          controller: _phoneController,
+                          focusNode: _focusNodeOne,
+                          keyboardType: TextInputType.phone,
+                          decoration: InputDecoration(
+                            prefix: Text("$countryCode   "),
+                            labelText: 'Mobile No.',
+                            labelStyle: _focusNodeOne.hasFocus
+                                ? regularBlueColor()
+                                : regulargreyColor(),
+                            //  errorText: validateMobile.toString()
+                          ),
                         ),
-                      ),
 
-                      /// Password*************************************************************************************
-                      // TextFormField(
-                      //   validator: validatePassword,
-                      //   autofocus: false,
-                      //   focusNode: _focusNodeTwo,
-                      //   decoration: InputDecoration(
-                      //     suffixIcon: IconButton(
-                      //         icon: passwordVisiable == true
-                      //             ? Icon(Icons.visibility)
-                      //             : Icon(Icons.visibility_off),
-                      //         onPressed: () {
-                      //           setState(() {
-                      //             passwordVisiable = !passwordVisiable;
-                      //           });
-                      //         }),
-                      //     labelText: 'Password',
-                      //     labelStyle: _focusNodeTwo.hasFocus
-                      //         ? regularBlueColor()
-                      //         : regulargreyColor(),
-                      //   ),
-                      //   obscureText: !passwordVisiable,
-                      // ),
+                        /// Password*************************************************************************************
+                        // TextFormField(
+                        //   validator: validatePassword,
+                        //   autofocus: false,
+                        //   focusNode: _focusNodeTwo,
+                        //   decoration: InputDecoration(
+                        //     suffixIcon: IconButton(
+                        //         icon: passwordVisiable == true
+                        //             ? Icon(Icons.visibility)
+                        //             : Icon(Icons.visibility_off),
+                        //         onPressed: () {
+                        //           setState(() {
+                        //             passwordVisiable = !passwordVisiable;
+                        //           });
+                        //         }),
+                        //     labelText: 'Password',
+                        //     labelStyle: _focusNodeTwo.hasFocus
+                        //         ? regularBlueColor()
+                        //         : regulargreyColor(),
+                        //   ),
+                        //   obscureText: !passwordVisiable,
+                        // ),
 
-                      // ///Forgot password***************************************************************************
-                      // GestureDetector(
-                      //   child: Container(
-                      //     alignment: Alignment.centerRight,
-                      //     child: Text(
-                      //       'Forgot Password?',
-                      //       style: smallPrimaryColorRegular(),
-                      //     ),
-                      //   ),
-                      //   onTap: () => Navigator.push(
-                      //       context,
-                      //       MaterialPageRoute(
-                      //           builder: (context) => ForgotPassword())),
-                      // ),
+                        // ///Forgot password***************************************************************************
+                        // GestureDetector(
+                        //   child: Container(
+                        //     alignment: Alignment.centerRight,
+                        //     child: Text(
+                        //       'Forgot Password?',
+                        //       style: smallPrimaryColorRegular(),
+                        //     ),
+                        //   ),
+                        //   onTap: () => Navigator.push(
+                        //       context,
+                        //       MaterialPageRoute(
+                        //           builder: (context) => ForgotPassword())),
+                        // ),
 
-                      /// Sign In button *************************************************************************
-                      FlatButton(
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(18.0)),
-                        color: primaryColor,
-                        textColor: whiteColor,
-                        onPressed: () {
-                          // if (_formKey.currentState.validate()) {
+                        /// Sign In button *************************************************************************
+                        FlatButton(
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(18.0)),
+                          color: primaryColor,
+                          textColor: whiteColor,
+                          onPressed: () {
+                            // if (_formKey.currentState.validate()) {
 
-                          // }
-                          final phoneNumber =
-                              countryCode + _phoneController.text.trim();
-                          print(phoneNumber);
-                          loginUser(phoneNumber, context);
-                        },
-                        child: Text(
-                          'Sign In',
-                          style: regularwhiteColorBold(),
-                        ),
-                      ),
-
-                      Container(
-                          alignment: Alignment.center,
+                            // }
+                            setState(() {
+                              showSpinner = true;
+                            });
+                            final phoneNumber =
+                                countryCode + _phoneController.text.trim();
+                            print(phoneNumber);
+                            loginUser(phoneNumber, context);
+                            setState(() {
+                              showSpinner = false;
+                            });
+                          },
                           child: Text(
-                            'OR',
-                            style: regularprimaryColorBold(),
-                          )),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceAround,
-                        children: [
-                          Container(
-                            height: 50,
-                            width: 100,
-                            child: Image.asset('lib/assets/images/google.png',
-                                fit: BoxFit.contain),
+                            'Sign In',
+                            style: regularwhiteColorBold(),
                           ),
-                          Container(
-                            height: 45,
-                            width: 80,
-                            child: Image.asset(
-                                'lib/assets/images/facebookLogo.png',
-                                fit: BoxFit.contain),
-                          ),
-                        ],
-                      ),
-                      GestureDetector(
-                          onTap: () =>
-                              Navigator.popAndPushNamed(context, '/signUp'),
-                          child: Container(
-                              alignment: Alignment.center,
-                              child: RichText(
-                                text: TextSpan(
-                                    text: 'Don\'t have an account?',
-                                    style: smallBlackColorRegular(),
-                                    children: [
-                                      TextSpan(
-                                          text: 'Sign Up',
-                                          style: smallPrimaryColorSemiBold())
-                                    ]),
-                              )))
-                    ],
-                  ),
-                ))));
+                        ),
+
+                        Container(
+                            alignment: Alignment.center,
+                            child: Text(
+                              'OR',
+                              style: regularprimaryColorBold(),
+                            )),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: [
+                            GestureDetector(
+                              onTap: signInWithGoogle,
+                              child: Container(
+                                height: 50,
+                                width: 100,
+                                child: Image.asset(
+                                    'lib/assets/images/google.png',
+                                    fit: BoxFit.contain),
+                              ),
+                            ),
+                            GestureDetector(
+                              onTap: () {
+                                signInWithFacebook();
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) => HomeScreen(),
+                                  ),
+                                );
+                              },
+                              child: Container(
+                                height: 45,
+                                width: 80,
+                                child: Image.asset(
+                                    'lib/assets/images/facebookLogo.png',
+                                    fit: BoxFit.contain),
+                              ),
+                            ),
+                          ],
+                        ),
+                        GestureDetector(
+                            onTap: () {
+                              print(_auth.currentUser);
+
+                              signOutUser();
+                              print('Sign out');
+                              print(_auth.currentUser);
+                            },
+                            child: Container(
+                                alignment: Alignment.center,
+                                child: RichText(
+                                  text: TextSpan(
+                                      text: 'Don\'t have an account?',
+                                      style: smallBlackColorRegular(),
+                                      children: [
+                                        TextSpan(
+                                            text: 'Sign Out',
+                                            style: smallPrimaryColorSemiBold())
+                                      ]),
+                                )))
+                      ],
+                    ),
+                  ))),
+        ));
   }
 
   ///mobile number validation***********************************************************************
