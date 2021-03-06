04926bdc4a9b9bbeee090918e9817266efeed83a
shivani savsani <shivanisavsani1711@gmail.com>
2021-03-06 23:30:36 +0530

    Add new job page completed

--- employment_app/lib/screen/addJob/Preview.dart
+++ employment_app/lib/screen/addJob/Preview.dart
@@ -1,3 +1,4 @@
+import 'package:employment_app/home_screen.dart';
 import 'package:employment_app/style/Style.dart';
 import 'package:flutter/material.dart';
 
@@ -30,26 +31,139 @@ class _PreviewState extends State<Preview> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        automaticallyImplyLeading: false,
-        backgroundColor: primaryColor,
-        title: Text('Preview'),
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Text(widget.fristName),
-            Text(widget.lastName),
-            Text(widget.mobileNo),
-            Text(widget.email),
-            Text(widget.address),
-            Text(widget.jobType),
-            Text(widget.discription),
-            Text(widget.noOfPeople),
-            Text(widget.time)
-          ],
+        appBar: AppBar(
+          automaticallyImplyLeading: false,
+          backgroundColor: primaryColor,
+          title: Text('Preview'),
         ),
-      ),
-    );
+        body: SingleChildScrollView(
+          child: Container(
+            margin: EdgeInsets.fromLTRB(
+                screenWidth(context) * 0.05,
+                screenHeight(context) * 0.05,
+                screenWidth(context) * 0.05,
+                screenHeight(context) * 0.05),
+            height: screenHeight(context) * 0.75,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                RichText(
+                  text: TextSpan(
+                      text: 'Frist name: ',
+                      style: largePrimaryColorsemiBold(),
+                      children: [
+                        TextSpan(
+                            text: '${widget.fristName}',
+                            style: regularBlackColorRegular())
+                      ]),
+                ),
+                RichText(
+                  text: TextSpan(
+                      text: 'Last name: ',
+                      style: largePrimaryColorsemiBold(),
+                      children: [
+                        TextSpan(
+                            text: '${widget.lastName}',
+                            style: regularBlackColorRegular())
+                      ]),
+                ),
+                RichText(
+                  text: TextSpan(
+                      text: 'Mobile no: ',
+                      style: largePrimaryColorsemiBold(),
+                      children: [
+                        TextSpan(
+                            text: '${widget.mobileNo}',
+                            style: regularBlackColorRegular())
+                      ]),
+                ),
+                RichText(
+                  text: TextSpan(
+                      text: 'Email: ',
+                      style: largePrimaryColorsemiBold(),
+                      children: [
+                        TextSpan(
+                            text: '${widget.email}',
+                            style: regularBlackColorRegular())
+                      ]),
+                ),
+                RichText(
+                  text: TextSpan(
+                      text: 'Address: ',
+                      style: largePrimaryColorsemiBold(),
+                      children: [
+                        TextSpan(
+                            text: '${widget.address}',
+                            style: regularBlackColorRegular())
+                      ]),
+                ),
+                RichText(
+                  text: TextSpan(
+                      text: 'Job type: ',
+                      style: largePrimaryColorsemiBold(),
+                      children: [
+                        TextSpan(
+                            text: '${widget.jobType}',
+                            style: regularBlackColorRegular())
+                      ]),
+                ),
+                RichText(
+                  text: TextSpan(
+                      text: 'Discription: ',
+                      style: largePrimaryColorsemiBold(),
+                      children: [
+                        TextSpan(
+                            text: '${widget.discription}',
+                            style: regularBlackColorRegular())
+                      ]),
+                ),
+                RichText(
+                  text: TextSpan(
+                      text: 'No of people : ',
+                      style: largePrimaryColorsemiBold(),
+                      children: [
+                        TextSpan(
+                            text: '${widget.noOfPeople}',
+                            style: regularBlackColorRegular())
+                      ]),
+                ),
+                RichText(
+                  text: TextSpan(
+                      text: 'Time: ',
+                      style: largePrimaryColorsemiBold(),
+                      children: [
+                        TextSpan(
+                            text: '${widget.time}',
+                            style: regularBlackColorRegular())
+                      ]),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    FlatButton(
+                        color: primaryColor,
+                        onPressed: () => Navigator.pop(context),
+                        child: Text(
+                          'Edit'.toUpperCase(),
+                          style: largewhiteColorBold(),
+                        )),
+                    FlatButton(
+                        color: primaryColor,
+                        onPressed: () => Navigator.pushAndRemoveUntil(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => HomeScreen()),
+                            (route) => false),
+                        child: Text(
+                          'Submit'.toUpperCase(),
+                          style: largewhiteColorBold(),
+                        ))
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ));
   }
 }
