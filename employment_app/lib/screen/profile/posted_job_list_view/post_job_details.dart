4451687e7d26936800dfb1da407732339e9d1c15
Fenil <fenilm57@gmail.com>
2021-03-27 15:03:20 +0530

    progessbar

--- employment_app/lib/screen/profile/posted_job_list_view/post_job_details.dart
+++ employment_app/lib/screen/profile/posted_job_list_view/post_job_details.dart
@@ -5,6 +5,7 @@ import 'package:employment_app/services/database.dart';
 import 'package:employment_app/style/Style.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
+import 'package:modal_progress_hud/modal_progress_hud.dart';
 import 'package:provider/provider.dart';
 
 // List for fetching data from firebase
@@ -14,6 +15,7 @@ var jobDescription = [];
 var numberOfPeople = [];
 var hours = [];
 var address = [];
+bool spinKit = false;
 
 class JobPostDetails extends StatefulWidget {
   final int count;
@@ -44,126 +46,136 @@ class _JobPostDetailsState extends State<JobPostDetails> {
           backgroundColor: primaryColor,
           title: Text('Job description'),
         ),
-        body: SingleChildScrollView(
-          child: Container(
-            margin: EdgeInsets.fromLTRB(
-                screenWidth(context) * 0.05,
-                screenHeight(context) * 0.05,
-                screenWidth(context) * 0.05,
-                screenHeight(context) * 0.05),
-            height: screenHeight(context) * 0.75,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              mainAxisSize: MainAxisSize.max,
-              children: [
-                RichText(
-                  text: TextSpan(
-                      text: 'Title: ',
-                      style: largePrimaryColorsemiBold(),
-                      children: [
-                        TextSpan(
-                            text: jobTitle[widget.count],
-                            style: regularBlackColorRegular())
-                      ]),
-                ),
-                RichText(
-                  text: TextSpan(
-                      text: 'No of pepole needed:',
-                      style: largePrimaryColorsemiBold(),
-                      children: [
-                        TextSpan(
-                            text: numberOfPeople[widget.count],
-                            style: regularBlackColorRegular())
-                      ]),
-                ),
-                RichText(
-                  text: TextSpan(
-                      text: 'Hours:',
-                      style: largePrimaryColorsemiBold(),
-                      children: [
-                        TextSpan(
-                            text: hours[widget.count],
-                            style: regularBlackColorRegular())
-                      ]),
-                ),
-                RichText(
-                  text: TextSpan(
-                      text: 'Description:',
-                      style: largePrimaryColorsemiBold(),
-                      children: [
-                        TextSpan(
-                            text: jobDescription[widget.count],
-                            style: regularBlackColorRegular())
-                      ]),
-                ),
-                RichText(
-                  text: TextSpan(
-                      text: 'Address:',
-                      style: largePrimaryColorsemiBold(),
-                      children: [
-                        TextSpan(
-                            text: address[widget.count],
-                            style: regularBlackColorRegular())
-                      ]),
-                ),
+        body: ModalProgressHUD(
+          inAsyncCall: spinKit,
+          child: SingleChildScrollView(
+            child: Container(
+              margin: EdgeInsets.fromLTRB(
+                  screenWidth(context) * 0.05,
+                  screenHeight(context) * 0.05,
+                  screenWidth(context) * 0.05,
+                  screenHeight(context) * 0.05),
+              height: screenHeight(context) * 0.75,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  RichText(
+                    text: TextSpan(
+                        text: 'Title: ',
+                        style: largePrimaryColorsemiBold(),
+                        children: [
+                          TextSpan(
+                              text: jobTitle[widget.count],
+                              style: regularBlackColorRegular())
+                        ]),
+                  ),
+                  RichText(
+                    text: TextSpan(
+                        text: 'No of pepole needed:',
+                        style: largePrimaryColorsemiBold(),
+                        children: [
+                          TextSpan(
+                              text: numberOfPeople[widget.count],
+                              style: regularBlackColorRegular())
+                        ]),
+                  ),
+                  RichText(
+                    text: TextSpan(
+                        text: 'Hours:',
+                        style: largePrimaryColorsemiBold(),
+                        children: [
+                          TextSpan(
+                              text: hours[widget.count],
+                              style: regularBlackColorRegular())
+                        ]),
+                  ),
+                  RichText(
+                    text: TextSpan(
+                        text: 'Description:',
+                        style: largePrimaryColorsemiBold(),
+                        children: [
+                          TextSpan(
+                              text: jobDescription[widget.count],
+                              style: regularBlackColorRegular())
+                        ]),
+                  ),
+                  RichText(
+                    text: TextSpan(
+                        text: 'Address:',
+                        style: largePrimaryColorsemiBold(),
+                        children: [
+                          TextSpan(
+                              text: address[widget.count],
+                              style: regularBlackColorRegular())
+                        ]),
+                  ),
 
-                Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      FlatButton.icon(
-                          color: primaryColor,
-                          onPressed: () {},
-                          icon: Icon(
-                            Icons.update,
-                            color: Colors.white,
-                          ),
-                          label: Text(
-                            'Update'.toUpperCase(),
-                            style: largewhiteColorBold(),
-                          )),
-                      FlatButton.icon(
-                          icon: Icon(
-                            Icons.delete,
-                            color: Colors.white,
-                          ),
-                          label: Text(
-                            'Delete'.toUpperCase(),
-                            style: largewhiteColorBold(),
-                          ),
-                          color: primaryColor,
-                          onPressed: () async {
-                            // Delete that post
-                            // ignore: unnecessary_statements
-                            databaseService.jobId();
-                            databaseService.display();
-                            print('Count:${widget.count}');
+                  Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        FlatButton.icon(
+                            color: primaryColor,
+                            onPressed: () {},
+                            icon: Icon(
+                              Icons.update,
+                              color: Colors.white,
+                            ),
+                            label: Text(
+                              'Update'.toUpperCase(),
+                              style: largewhiteColorBold(),
+                            )),
+                        FlatButton.icon(
+                            icon: Icon(
+                              Icons.delete,
+                              color: Colors.white,
+                            ),
+                            label: Text(
+                              'Delete'.toUpperCase(),
+                              style: largewhiteColorBold(),
+                            ),
+                            color: primaryColor,
+                            onPressed: () async {
+                              // Delete that post
+                              // ignore: unnecessary_statements
+                              databaseService.jobId();
+                              databaseService.display();
+                              print('Count:${widget.count}');
 
-                            await databaseService
-                                .deleteJobDetails(widget.count);
+                              // setState(() {
+                              //   spinKit = true;
+                              // });
 
-                            Fluttertoast.showToast(
-                              backgroundColor: primaryColor,
-                              textColor: Colors.white,
-                              gravity: ToastGravity.BOTTOM,
-                              toastLength: Toast.LENGTH_LONG,
-                              msg: 'Deleted suceessfully',
-                            );
-                            setState(() {
-                              Navigator.pushAndRemoveUntil(
-                                  context,
-                                  MaterialPageRoute(
-                                    builder: (context) => HomeScreen(
-                                      flag: 0,
+                              await databaseService
+                                  .deleteJobDetails(widget.count);
+                              setState(() {
+                                spinKit = false;
+                              });
+
+                              Fluttertoast.showToast(
+                                backgroundColor: primaryColor,
+                                textColor: Colors.white,
+                                gravity: ToastGravity.BOTTOM,
+                                toastLength: Toast.LENGTH_LONG,
+                                msg: 'Deleted suceessfully',
+                              );
+                              setState(() {
+                                Navigator.pushAndRemoveUntil(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (context) => HomeScreen(
+                                        flag: 0,
+                                      ),
                                     ),
-                                  ),
-                                  (route) => false);
-                            });
-                          }),
-                    ])
-                //        )
-                //)
-              ],
+                                    (route) => false);
+                              });
+                            }),
+                      ])
+                  //        )
+                  //)
+                ],
+              ),
             ),
           ),
         ));
