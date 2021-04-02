6ceb7c4ed07f8ae2cb0182e27aefb2ab3f25f6c7
shivani savsani <shivanisavsani1711@gmail.com>
2021-04-03 00:26:42 +0530

    job provider view ui

--- employment_app/lib/screen/job_details/job_provider_view.dart
+++ employment_app/lib/screen/job_details/job_provider_view.dart
@@ -14,6 +14,50 @@ class _JobProviderViewState extends State<JobProviderView> {
         backgroundColor: primaryColor,
         title: Text('Provider Details'),
       ),
+      body: Container(
+        margin: EdgeInsets.fromLTRB(0, 45, 0, 0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+              CircleAvatar(
+                radius: 85,
+                backgroundColor: Colors.white,
+                backgroundImage:
+                AssetImage('lib/assets/images/user_avatar.png'),
+              ),
+            ],),
+          Container(
+            margin: EdgeInsets.only(left: 20,top: 20),
+              height: screenHeight(context)*0.1,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+            RichText(
+              text: TextSpan(
+                  text: 'First name: ',
+                  style: largePrimaryColorsemiBold(),
+                  children: [
+                    TextSpan(
+                        text: '', style: regularBlackColorRegular())
+                  ]),
+            ),
+            RichText(
+              text: TextSpan(
+                  text: 'Mobile no: ',
+                  style: largePrimaryColorsemiBold(),
+                  children: [
+                    TextSpan(
+                        text: '', style: regularBlackColorRegular())
+                  ]),
+            ),
+          ],))
+
+        ],),
+      ),
     );
   }
 }
